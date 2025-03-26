f50f7ef5c7359d59f754b1dc9422dd77273e00a3
Nirawit Jongprasartsuk <nirawitrordor@gmail.com>
2025-03-26 14:41:46 +0700

    Adding Style to settings (use css) and restyle some main menu

--- src/scene/SettingsState.java
+++ src/scene/SettingsState.java
@@ -2,6 +2,7 @@ package scene;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.VBox;
 import manager.SceneManager;
@@ -9,6 +10,8 @@ import manager.SceneManager;
 public class SettingsState implements SceneState {
     @Override
     public void start(SceneManager sceneManager) {
+    	// Volume Slider
+    	Label volumeLabel = new Label("Volume");
         Slider volumeSlider = new Slider(0, 1, 1);
         volumeSlider.setOnMouseReleased(e -> {
             // Save the volume setting or apply it
@@ -17,11 +20,18 @@ public class SettingsState implements SceneState {
         
         Button backButton = new Button("Back to Main Menu");
         backButton.setOnAction(e -> sceneManager.setState(new MainMenuState()));
-
-        VBox root = new VBox(volumeSlider, backButton);
+        
+        VBox volume = new VBox(volumeLabel, volumeSlider);
+        volume.setSpacing(5);
+        volume.setStyle("-fx-alignment: center;");
+        VBox root = new VBox(volume, backButton);
         root.setStyle("-fx-alignment: center;");
-        root.setSpacing(20);
-        sceneManager.getStage().setScene(new Scene(root, 800, 600));
+        root.setSpacing(50);
+        String cssPath = getClass().getClassLoader().getResource("css/SettingMenu.css").toExternalForm();
+        Scene scene = new Scene(root, 800, 600);
+        System.out.println("Loading CSS from: " + cssPath);
+        scene.getStylesheets().add(cssPath);
+        sceneManager.getStage().setScene(scene);
         sceneManager.getStage().setTitle("Settings");
         sceneManager.getStage().show();
     }
